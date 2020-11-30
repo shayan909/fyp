3ad9f3e77d25d884ae5365befdd45a585662a37a
shayan <shayan_90@live.com>
2020-11-30 21:08:34 +0500

    question generation alpha1

--- NLP.py
+++ NLP.py
@@ -1,15 +1,19 @@
-symptoms = {"fever": {"question": {"duration": "How long you're having it?", "severity": "What is the temperature?"}}}
+symptoms = {"fever": {"question": {"duration": "How long you're having it?", "severity": "What is the temperature?"}},
+            "cough": {"question": {"duration": "How long you're having cough?", "severity": "is it dry or wet?"}}}
 
-symplst = ["fever"]
+symplst = ["fever","cough"]
 symp_details = {}
+my_symp = {}
 
 for x in symplst:
-    if x in symptoms.keys():
+    #if x in symptoms.keys():
         print(symptoms[x]["question"]["duration"])
         symp_details["duration"] = input()
         print(symp_details)
         print(symptoms[x]["question"]["severity"])
-        symp_details[x] = input()
-    else:
+        symp_details["severity"] = input()
+        my_symp[x] = symp_details
+        print(my_symp)
+   # else:
         print("no match")
 
