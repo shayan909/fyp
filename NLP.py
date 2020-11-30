be5e9f102bba6d52fb4b5d2dc69f8469542be252
shayan <shayan_90@live.com>
2020-11-30 20:49:15 +0500

    question generation start

--- NLP.py
+++ NLP.py
@@ -1,7 +1,15 @@
-userInput= input("Enter symptoms")
-symplst = []
+symptoms = {"fever": {"question": {"duration": "How long you're having it?", "severity": "What is the temperature?"}}}
 
-symplst.append(userInput)
-print(symplst)
+symplst = ["fever"]
+symp_details = {}
 
+for x in symplst:
+    if x in symptoms.keys():
+        print(symptoms[x]["question"]["duration"])
+        symp_details["duration"] = input()
+        print(symp_details)
+        print(symptoms[x]["question"]["severity"])
+        symp_details[x] = input()
+    else:
+        print("no match")
 
