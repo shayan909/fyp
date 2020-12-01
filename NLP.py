14ac9e5a2ed99591bcfcda1b9acb09f378d37317
shayan <shayan_90@live.com>
2020-12-01 17:18:24 +0500

    question generation beta

--- NLP.py
+++ NLP.py
@@ -3,19 +3,22 @@ import json
 with open('intents.json') as file:
     data = json.load(file)
 
+critical_symptoms = ["fever","cough","chest discomfort","chest pain","wheezing","sore throat","headache", "loss of smell"];
 
-symplst = ["fever","chest pain"]
+symplst = ["fever"]
 my_symp = {}
-
-for x in symplst:
-        symp_details = {}
-
-        print(data["symptoms"][x]["question"]["duration"])
-        symp_details["duration"] = input()
-        print(symp_details)
-        print(data["symptoms"][x]["question"]["severity"])
-        symp_details["severity"] = input()
-        my_symp[x] = symp_details
+if symplst.sort() == critical_symptoms.sort():
+        for x in symplst:
+                symp_details = {}
+                if x in data["symptoms"]:
+                        print(data["symptoms"][x]["question"]["duration"])
+                        choice = int(input())
+                        symp_details["duration"]=data["symptoms"][x]["question"]["answer1"][choice-1]
+                        print(symp_details)
+                        print(data["symptoms"][x]["question"]["severity"])
+                        choice2 = int(input())
+                        symp_details["severity"] = data["symptoms"][x]["question"]["answer2"][choice2 - 1]
+                        my_symp[x] = symp_details
 
 print(my_symp)
 
